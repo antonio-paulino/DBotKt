e4c7adc2585170a91864fd9ae2e3ba6ec9e6349b
António Paulino <antonioppaulino2@gmail.com>
2025-07-19 12:46:27 +0100

    Reformat

--- src/main/kotlin/pt/paulinoo/dbotkt/player/listeners/VoiceChannelEmptyListener.kt
+++ src/main/kotlin/pt/paulinoo/dbotkt/player/listeners/VoiceChannelEmptyListener.kt
@@ -1,14 +1,14 @@
 package pt.paulinoo.dbotkt.player.listeners
 
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.Member
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
-import net.dv8tion.jda.api.entities.Member
-import net.dv8tion.jda.api.entities.Guild
-import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel
 import pt.paulinoo.dbotkt.player.audio.AudioManager
 
 class VoiceChannelEmptyListener(
-    private val audioCommandManager: AudioManager
+    private val audioCommandManager: AudioManager,
 ) : ListenerAdapter() {
     override fun onGuildVoiceUpdate(event: GuildVoiceUpdateEvent) {
         val guild: Guild = event.guild
